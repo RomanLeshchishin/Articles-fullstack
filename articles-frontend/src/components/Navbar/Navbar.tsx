2cd0ff63f0991a408c8d4a40f8c0f130143248fe
Roman <romamail.ru365@gmail.com>
2024-04-30 20:27:24 +0500

    Frontend, add user name to Navbar

--- articles-frontend/src/components/Navbar/Navbar.tsx
+++ articles-frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 import styles from "./Navbar.module.scss";
 import {Link} from "react-router-dom";
+import {useAppSelector} from "../../hooks/redux.ts";
 
 const Navbar = () => {
+	const {user} = useAppSelector(state => state.authReducer)
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>Лого</div>
@@ -9,10 +11,12 @@ const Navbar = () => {
                 <Link to={''} className={styles.navLink}>Статьи</Link>
                 <Link to={''} className={styles.navLink}>Авторы</Link>
             </div>
-            <div className={styles.navLinkBlock}>
-                <Link to={''} className={styles.navLink}>Войти</Link>
-                <Link to={''} className={styles.navLink}>Зарегистрироваться</Link>
-            </div>
+			{user.name !== undefined ? user.name :
+				<div className={styles.navLinkBlock}>
+					<Link to={'/login'} className={styles.navLink}>Войти</Link>
+					<Link to={'/registration'} className={styles.navLink}>Зарегистрироваться</Link>
+				</div>
+			}
         </div>
     );
 };
